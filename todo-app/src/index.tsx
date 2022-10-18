81a861f22836300a8b6cf9b88b653636766baec3
MoSangBin <mosangbin94@gmail.com>
2022-10-18 13:59:37 +0900

    style: css 초기화

--- todo-app/src/index.tsx
+++ todo-app/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 
 import App from "./App";
+import { GlobalStyle } from "./assets";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -10,6 +11,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <GlobalStyle />
       <App />
     </BrowserRouter>
   </React.StrictMode>
