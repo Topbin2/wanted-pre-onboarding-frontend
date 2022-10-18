5084be7f8febc1e2bef6fcccd7eed77a33824171
MoSangBin <mosangbin94@gmail.com>
2022-10-18 19:47:39 +0900

    refactor: style 파일 분리

--- todo-app/src/pages/Login/index.tsx
+++ todo-app/src/pages/Login/index.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import styled from "styled-components";
 
 import { signIn, signUp } from "../../apis";
 import { Input } from "../../components";
@@ -10,73 +9,7 @@ import {
   getTokenFromLocalStorage,
   passwordValidation,
 } from "../../utils";
-
-export const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-export const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  gap: 20px;
-  width: 400px;
-  padding: 30px;
-  background-color: white;
-  border-radius: 0 0 7px 7px;
-  box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05),
-    0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
-`;
-
-export const Button = styled.button`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  height: 30px;
-  margin-top: 20px;
-  padding: 20px 0;
-  color: #ffffff;
-  background-color: #1561ed;
-  border: none;
-  border-radius: 20px;
-  transition: all 0.4s;
-
-  &:disabled {
-    opacity: 0.5;
-    cursor: default;
-  }
-`;
-
-export const Tab = styled.section<{ isLogin: boolean }>`
-  display: flex;
-  width: 400px;
-
-  & > button {
-    border: 1px solid black;
-    border-radius: 10px 10px 0 0;
-    flex-basis: 50%;
-    padding: 15px 0;
-    border: none;
-    box-shadow: 0 10px 24px hsla(0, 0%, 0%, 0.05),
-      0 20px 48px hsla(0, 0%, 0%, 0.05), 0 1px 4px hsla(0, 0%, 0%, 0.1);
-    transition: all 0.4s;
-  }
-
-  & > button:first-child {
-    background-color: ${({ isLogin }) => (isLogin ? "#1561ed" : "inherit")};
-    color: ${({ isLogin }) => (isLogin ? "#ffffff" : "#000000")};
-  }
-
-  & > button:last-child {
-    background-color: ${({ isLogin }) => (isLogin ? "inherit" : "#1561ed")};
-    color: ${({ isLogin }) => (isLogin ? "#000000" : "#ffffff")};
-  }
-`;
+import * as S from "./style";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -113,16 +46,16 @@ const Login = () => {
   }, [navigate]);
 
   return (
-    <Container>
-      <Tab isLogin={isLogin}>
+    <S.Container>
+      <S.Tab isLogin={isLogin}>
         <button type="button" onClick={() => setIsLogin(true)}>
           로그인
         </button>
         <button type="button" onClick={() => setIsLogin(false)}>
           회원가입
         </button>
-      </Tab>
-      <Form onSubmit={(e) => handleSubmit(e)}>
+      </S.Tab>
+      <S.Form onSubmit={(e) => handleSubmit(e)}>
         <Input
           type="text"
           label="이메일"
@@ -141,11 +74,11 @@ const Login = () => {
           errorMsg="비밀번호를 8글자 이상 입력해주세요."
           onChange={(e) => handlePassword(e)}
         />
-        <Button disabled={!isEmailValidate || !isPasswordValidate}>
+        <S.Button disabled={!isEmailValidate || !isPasswordValidate}>
           {isLogin ? "로그인" : "회원가입"}
-        </Button>
-      </Form>
-    </Container>
+        </S.Button>
+      </S.Form>
+    </S.Container>
   );
 };
 
