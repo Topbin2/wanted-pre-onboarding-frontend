bc9ec6251e6d083d4451472b61c5f4451110248a
MoSangBin <mosangbin94@gmail.com>
2022-10-18 13:40:16 +0900

    feat: 로그인, 회원가입, 투두 페이지 라우트 연결

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -1,5 +1,15 @@
+import { Route, Routes } from "react-router-dom";
+
+import { Login, Signup, Todo } from "./pages";
+
 const App = () => {
-  return <h1>하이</h1>;
+  return (
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/todo" element={<Todo />} />
+    </Routes>
+  );
 };
 
 export default App;
