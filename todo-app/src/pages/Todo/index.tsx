7b66a5a68dda3d8f5b192c1613ad750c20905c20
MoSangBin <mosangbin94@gmail.com>
2022-10-18 19:53:30 +0900

    feat: token 없이 todo 경로로 접속시 '/' 경로로 리다이렉트

--- todo-app/src/pages/Todo/index.tsx
+++ todo-app/src/pages/Todo/index.tsx
@@ -1,4 +1,19 @@
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import { getTokenFromLocalStorage } from "../../utils";
+
 const Todo = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const token = getTokenFromLocalStorage();
+
+    if (!token) {
+      navigate("/");
+    }
+  }, [navigate]);
+
   return <div>todo</div>;
 };
 
